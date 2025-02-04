7e7571e637716401439c8fa1da9dcb0d5426b10a
NFG-J <niels.f.garmann-johnsen@uia.no>
2025-02-04 14:59:10 +0100

    Add files via upload

--- excel_to_json2.py
+++ excel_to_json2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # Load the Excel file
-excel_file = 'C:/Users/nielsfgj/WebstormProjects/DesignIT_2.03/data/WebComponents (version 2025-01-09).xlsx'  # Update this to the file path
+excel_file = 'C:/Users/nielsfgj/WebstormProjects/DesignIT_2.04/data/WebComponents (version 2025-02-04).xlsx'  # Update this to the file path
 sheet_name = 'Components'  # Sheet name where data is located
 
 # Read the data from the specified sheet
@@ -13,7 +13,7 @@ df = pd.read_excel(excel_file, sheet_name=sheet_name)
 data = df[['Title', 'HTML', 'CSS', 'Reference']].to_dict(orient='records')
 
 # Save the data to a JSON file
-json_file_path = 'C:/Users/nielsfgj/WebstormProjects/DesignIT_2.03/data/components.json'  # Update this to desired output path
+json_file_path = 'C:/Users/nielsfgj/WebstormProjects/DesignIT_2.04/data/components.json'  # Update this to desired output path
 with open(json_file_path, 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, indent=4, ensure_ascii=False)
 
